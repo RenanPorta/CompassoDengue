83942659d4e16d3feacad82df075b06835d76d3a
José Jacinto <jose.moraes_BOLS@compasso.com.br>
2020-06-15 01:28:40 -0300

    Corrigindo erros pagina visita
    Como corrigi alguns detalhes no database.js é necessário dar um "drop" no banco dengue e criar-lo novamente, para criar corretamente as tabelas.

--- src/app/infra/visita-dao.js
+++ src/app/infra/visita-dao.js
@@ -3,36 +3,37 @@ const conexao = require('../../config/conexao/conexaoDatabase');
 class VisitaDAO {
     adiciona(visita, res) {
         const sql = `INSERT INTO visita SET ?`
-        const novaVisita = {codAtividade: visita.codAtividade,
-                            codTipoVisita: visita.codTipoVisita,
-                            codMunicipio: visita.codMunicipio,
-                            municipio: visita.municipio,
-                            area: visita.area,
-                            setorCensitario: visita.setorCensitario,
-                            quarteirao: visita.quarteirao,
-                            execucao: visita.execucao,
-                            dataVisita: visita.dataVisita,
-                            numCadOrdem: visita.numCadOrdem,
-                            endereco: visita.endereco,
-                            numComplemento: visita.numComplemento,
-                            codSituacaoImovel: visita.codSituacaoImovel,
-                            codTipoRecipientes: visita.codTipoRecipientes,
-                            codSituacaoRecipientes: visita.codSituacaoRecipientes,
-                            contrMecanico: visita.contrMecanico,
-                            codTratQuimico: visita.codTratQuimico,
-                            amostra: visita.amostra,
-                            codLarvas: visita.codLarvas,
-                            progRetorno: visita.progRetorno,
-                            codLaboratorio: visita.codLaboratorio,
-                            cpfAgente: visita.cpfAgente                            
-                            }
+        const novaVisita = {
+            codAtividade: visita.codAtividade,
+            codTipoVisita: visita.codTipoVisita,
+            codMunicipio: visita.codMunicipio,
+            municipio: visita.municipio,
+            area: visita.area,
+            setorCensitario: visita.setorCensitario,
+            quarteirao: visita.quarteirao,
+            execucao: visita.execucao,
+            dataVisita: visita.dataVisita,
+            numCadOrdem: visita.numCadOrdem,
+            endereco: visita.endereco,
+            numComplemento: visita.numComplemento,
+            trabalhado: visita.trabalhado,
+            codPendencia: visita.codPendencia,
+            codTipoRecipientes: visita.codTipoRecipientes,
+            codSituacaoRecipientes: visita.codSituacaoRecipientes,
+            contrMecanico: visita.contrMecanico,
+            codTratQuimico: visita.codTratQuimico,
+            amostra: visita.amostra,
+            codLarvas: visita.codLarvas,
+            progRetorno: visita.progRetorno
+        }
         console.log(novaVisita);
         conexao.query(sql, novaVisita, (erro) => {
-            if(erro){
+            if (erro) {
                 res.status(400).json(erro);
-                console.log("Erro ao cadastrar Visita: "+erro);
+                console.log("Erro ao cadastrar Visita: " + erro);
             } else {
-                const exibeVisita = {codAtividade: visita.codAtividade,
+                const exibeVisita = {
+                    codAtividade: visita.codAtividade,
                     codTipoVisita: visita.codTipoVisita,
                     codMunicipio: visita.codMunicipio,
                     municipio: visita.municipio,
@@ -44,23 +45,22 @@ class VisitaDAO {
                     numCadOrdem: visita.numCadOrdem,
                     endereco: visita.endereco,
                     numComplemento: visita.numComplemento,
-                    codSituacaoImovel: visita.codSituacaoImovel,
+                    trabalhado: visita.trabalhado,
+                    codPendencia: visita.codPendencia,
                     codTipoRecipientes: visita.codTipoRecipientes,
                     codSituacaoRecipientes: visita.codSituacaoRecipientes,
                     contrMecanico: visita.contrMecanico,
                     codTratQuimico: visita.codTratQuimico,
                     amostra: visita.amostra,
                     codLarvas: visita.codLarvas,
-                    progRetorno: visita.progRetorno,
-                    codLaboratorio: visita.codLaboratorio,
-                    cpfAgente: visita.cpfAgente                                      
-                    }
+                    progRetorno: visita.progRetorno
+                }
                 res.status(201).json(exibeVisita);
                 console.log(exibeVisita)
             }
         })
-        
+
     }
 }
-    
+
 module.exports = new VisitaDAO;
